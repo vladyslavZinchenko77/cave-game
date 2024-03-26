409fdf8bb9f223606d5ce913f4159fbdc57929eb
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-26 14:04:25 +0200

    added get token to startgame function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import './App.scss';
+import { useState } from 'react';
+
+import { getToken } from './services/gameService';
+import { useGameState } from './hooks/useGameState';
 import StartGameForm from './components/StartGame/StartGameForm';
+import './App.scss';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const { resetGame } = useGameState();
+
+  const handleStartGame = async (id: string) => {
+    setLoading(true);
+    resetGame();
+
+    try {
+      const token = await getToken(id);
+    } catch (error) {
+      console.error('Failed to start game:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
-      <StartGameForm />
-    </>
+    <>{loading ? 'loading' : <StartGameForm onStartGame={handleStartGame} />}</>
   );
 }
 
--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -1,18 +1,32 @@
+
+
 const BASE_URL = 'https://cave-drone-server.shtoa.xyz';
 
 export const initGame = async (name: string, complexity: number) => {
-    const response = await fetch(`${BASE_URL}/init`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, complexity }),
-    });
-  
-    if (!response.ok) {
-      throw new Error('Failed to initialize game');
-    }
-  
-    const { id } = await response.json();
-    return id;
-  };
+  const response = await fetch(`${BASE_URL}/init`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, complexity }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to initialize game');
+  }
+
+  const { id } = await response.json();
+  return id;
+};
+
+export const getToken = async (id: string) => {
+  const promises = Array.from({ length: 4 }, (_, i) => i + 1).map(async (chunkNo) => {
+    const response = await fetch(`${BASE_URL}/token/${chunkNo}?id=${id}`);
+    const { chunk } = await response.json();
+    return chunk;
+  });
+
+  const chunks = await Promise.all(promises);
+  return chunks.join('');
+};
+
